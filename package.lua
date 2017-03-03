46335ce8c5e41c3b1cff616a22f22e0e997c6e7b
Tim Caswell <tim@creationix.com>
2017-03-02 21:39:13 -0600

    Update version to 3.0.0

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "creationix/hexes",
-  version = "2.0.5",
+  version = "3.0.0",
   description = "An example luvi app using web sockets",
   homepage = "https://github.com/creationix/hexes",
   tags = {"game", "example"},
